f00a618329d88602ea23e3e319fd775433678c51
danielfc <0619988275@uma.es>
2022-02-25 11:11:01 +0100

    Añadido cambios al compute y pom

--- src/main/java/org/example/factorial/Factorial.java
+++ src/main/java/org/example/factorial/Factorial.java
@@ -3,11 +3,18 @@ package org.example.factorial;
 public class Factorial {
 
     public int compute(int value) {
+        if(value<0){
+            throw new RuntimeException("The value is negative: "+value);
+        }
         int result;
         if((value==0) || (value==1)){
             result=1;
-        }else{
+        }else if(value==2){
             result=2;
+        }else if(value==3){
+            result=6;
+        }else{
+            result = value *compute(value-1);
         }
         return result;
     }
